b2fcdceed4171f7c2d1400dc844840976eed5ed5
Kvadukul <kushal.vadukul2006@gmail.com>
2025-09-09 19:25:55 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -5,10 +5,14 @@ URL = "https://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter city name: ")
 
-url = f"{URL}?q={city}&appid={API}&units=metric"
+city_name = city
+api_key = API
+units = "metric"
+url = URL + "?q=" + city_name + "&appid=" + api_key + "&units=" + units
 
 response = requests.get(url)
 data = response.json()
+print (data)
 
 if data["cod"] == 200:
     name = data["name"]
