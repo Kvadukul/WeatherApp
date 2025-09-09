d0dc6d17597ec272719576b0c5906854c22cc65e
Kvadukul <kushal.vadukul2006@gmail.com>
2025-09-09 20:38:58 +0100

    Final version of Weather App with forecast feature
    Just added the forecast option where users can view the forecast in that city for the next 5 day if they wish to do so.

--- weather.py
+++ weather.py
@@ -2,6 +2,7 @@ import requests
 
 API = "b407855f9e57ea1e496313df9a905f9f"
 URL = "https://api.openweathermap.org/data/2.5/weather"
+ForecastURL = "https://api.openweathermap.org/data/2.5/forecast"
 
 city = input("Enter city name: ")
 
@@ -19,8 +20,26 @@ if data["cod"] == 200:
     desc = data["weather"][0]["description"]
     sea = data["main"]["sea_level"]
     wind = data["wind"]["speed"]
-    
     print(f"Weather in {name}: {temp}°C, {desc}, Sea Level is {sea}, Wind Speed is {wind}m/s")
+    
+    option= input("Would you like to see the 5 day forecast?  (y/n) ").lower()
+    if option == "y":
+        url = ForecastURL + "?q=" + city_name + "&appid=" + api_key + "&units=" + units
+        forecast_response = requests.get(ForecastURL)
+        forecast_data = forecast_response.json()
+        print(f"\n5-Day Forecast for {name}:\n")
+        
+        for entry in forecast_data["list"]:
+            if entry["dt_txt"].endswith("12:00:00"): 
+                date = entry["dt_txt"][:10]
+                temp = entry["main"]["temp"]   
+                print(f"{date}: {temp}°C")
+            else:
+                print("\nOk")
+
+
+
+
 elif data["cod"] == 404:
     print("City not found.")
 else:
